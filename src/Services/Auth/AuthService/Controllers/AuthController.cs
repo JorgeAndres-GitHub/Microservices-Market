dbb08234e02cf2a4282ec7137c281f95c371eff9
Jorge Andres <jorgeandresherreramonsalve@gmail.com>
2026-01-27 20:59:52 -0500

    Creating model architecture

---
+++ src/Services/Auth/AuthService/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register()
+        {
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login()
+        {
+            
+        }
+    }
+}
